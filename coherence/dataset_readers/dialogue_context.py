ec0f29ba2d6e794759b4adf9c1156284bcbb225b
Yannis Konstas <sinantie@gmail.com>
2018-05-20 11:28:34 -0700

    started adding attention to hierarchical encoder

--- coherence/dataset_readers/dialogue_context.py
+++ coherence/dataset_readers/dialogue_context.py
@@ -43,7 +43,7 @@ class DialogueContextDatasetReader(DatasetReader):
     def _read_file(self, file_path, label):
         examples = []
         with open(cached_path(file_path), "r") as data_file:
-            logger.info("Reading instances from file: %s", file_path, label)
+            logger.info("Reading instances from file: %s", file_path)
             for line_num, line in enumerate(tqdm.tqdm(data_file.readlines())):
                 line = line.strip("\n")
                 if not line:
