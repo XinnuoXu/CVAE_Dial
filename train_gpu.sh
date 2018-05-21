c16a4b9672e038dc13f033a2375760fa26af9260
Ondrej Dusek <o.dusek@hw.ac.uk>
2018-05-21 23:50:00 +0100

    Better job submission

--- train_gpu.sh
+++ train_gpu.sh
@@ -1,2 +1,3 @@
-srun --mem 400G --gres=gpu --partition=amd-longq --nodes 1 sh train.sh
-#srun --mem 20G --partition intel-longq --gres=gpu --nodelist=dgx01 sh train.sh
+#!/bin/bash
+#srun --mem 400G --gres=gpu --partition=amd-longq --nodes 1 sh train.sh
+sbatch -o train.%J.log -J XCVAE --mem 32G --partition intel-longq --gres=gpu --nodelist=dgx01 train.sh
